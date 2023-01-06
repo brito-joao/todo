1dfde29eb5e399a3610108f137bc04a2af079d7c
João <joaobrit0000000000@gmail.com>
2023-01-06 22:57:48 +0000

    Add note removing functionality

--- src/user_inputs.js
+++ src/user_inputs.js
@@ -30,7 +30,20 @@ export function getForm(user){
 
 
 export function displayNotes(note_to_display){
-    const notes= document.querySelector(".notes");
-    notes.innerHTML+=`<div class='note'><p class='title'>${note_to_display.title}</p><p class='description'>${note_to_display.description}</p><div><p class='others'>${note_to_display.category}</p><p class='others'>${note_to_display.due}</p><p class='others'>${note_to_display.importance}</p></div><button '>remove</button></div>`;
+  const notes= document.querySelector(".notes");
+  notes.innerHTML+=`<div class='note ${"id-note"}'><p class='title'>${note_to_display.title}</p><p class='description'>${note_to_display.description}</p><div><p class='others'>${note_to_display.category}</p><p class='others'>${note_to_display.due}</p><p class='others'>${note_to_display.importance}</p></div><button class='button' value=${"id-note"}>remove</button></div>`;
+  deleteNotes();
 }
-
+export function deleteNotes(){
+  
+  const button=document.querySelector(".button");
+  button.addEventListener("click",()=>{
+    
+    let note_class=button.value;
+    const note=document.querySelector(`.${note_class}`);
+    console.log(note, "note correct");
+    note.remove();
+    //change this to the correct address;
+    removeNotes("testing2");
+  })
+}
